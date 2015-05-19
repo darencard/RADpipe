02fb8876794ce2f361b218a7ff985abc70d899ed
darencard <dcard@uta.edu>
2015-05-19 17:43:18 -0500

    Initial commit. Script to generate initialization values for Entropy using genotype likelihood matrix.

---
+++ entropyStart.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript --vanilla --default-packages=base,utils,stats,MASS
+
+args<-commandArgs(TRUE)
+
+entropyStart <- function(infile, startk, endk, ...){
+#  library(MASS)
+  genoIn <- read.table(infile, header = FALSE, sep = " ", fill = TRUE)
+  rmCol <- ncol(genoIn) - 1
+  genoInTrim <- as.matrix(genoIn[-c(1:3), c(2:rmCol)])
+  tGenoInTrim <- t(genoInTrim)
+  class(tGenoInTrim) <- "numeric"
+
+  for (k in startk:endk){
+    pcaOut<-prcomp(tGenoInTrim,center=TRUE,scale=FALSE)
+    kMeanOut<-kmeans(pcaOut$x[,1:5],k,iter.max=10,nstart=10,algorithm="Hartigan-Wong")
+    ldaOut<-lda(x=pcaOut$x[,1:5],grouping=kMeanOut$cluster,CV=TRUE)
+    write.table(round(ldaOut$posterior,7),file=paste0("lda.k",k,".out"),quote=F,row.names=F,col.names=F)
+  }
+}
+
+entropyStart(args[1], args[2], args[3])
