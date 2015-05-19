3e9afa72d5c7e3f32892e1f7ab7e38479f9922f0
darencard <dcard@uta.edu>
2015-05-18 23:32:19 -0500

    Added flexibility to genotype likelihood matrix output.

--- genotype_from_VCF.py
+++ genotype_from_VCF.py
@@ -59,15 +59,20 @@ parser = optparse.OptionParser(usage = usage)
 parser.add_option("--samplesheet", action= "store", type = "string", dest = "sheet", help = "sample sheet containing samples being processed")
 parser.add_option("--vcf", action = "store", type = "string", dest = "vcf", help = "VCF input file")
 parser.add_option("--prefix", action = "store", dest = "prefix", help = "prefix for output files [out]", default = "out")
-parser.add_option("--entropy", action = "store_true", dest = "entropy", help = "create entropy input file and proper starting points [TRUE]", default = "True")
-parser.add_option("--startK", action = "store", dest = "sK", help = "starting (lower) K [1]", default = "1")
-parser.add_option("--endK", action = "store", dest = "eK", help = "ending (higher) K [5]", default = "2")
-parser.add_option("--nucl", action = "store_true", dest = "nucl", help = "create nucleotide FASTA alignment with IUPAC ambiguities for heterozygous sites [TRUE]", default = "True")
+#parser.add_option("--entropy", action = "store_true", dest = "entropy", help = "create entropy input file and proper starting points [TRUE]", default = "True")
+#parser.add_option("--startK", action = "store", dest = "sK", help = "starting (lower) K [1]", default = "1")
+#parser.add_option("--endK", action = "store", dest = "eK", help = "ending (higher) K [5]", default = "2")
+parser.add_option("--nucl", action = "store_true", dest = "nucl", help = "create nucleotide FASTA alignment with IUPAC ambiguities for heterozygous sites [TRUE]", default = True)
 parser.add_option("--trinary", action = "store_true", dest = "tri", help = "create trinary FASTA alignment with 0, 1, 2 genotype codes [TRUE]", default = "True")
+parser.add_option("--genotype", action = "store", dest = "genotype", help = "type of genotype likelihood output: 0 = PHRED, 1 = -Log10, 2 = Standardized, 3 = Genotype Uncertainty [0]", default = "0")
 parser.add_option("--gq", action = "store", dest = "gq", help = "threshold genotype quality for reporting individual genotype (otherwise coded as missing - ?) [20]", default = "20")
 parser.add_option("--ind", action = "store_true", dest = "ind", help = "thin dataset to only include 1 SNP per 10 kb, so as to reduce chance of linked SNPs [TRUE]", default = "True")
 parser.add_option("--thin", action = "store", dest = "thin", help = "window size to use for thinning in bp (keeps first SNP it finds and ignores others) [10000]", default = "10000")
 parser.add_option("--maf", action = "store", dest = "maf", help = "the minor allele frequency range desired: 0 (all MAF), 1 (MAF >= 0.050), 2 (0.010 <= MAF < 0.050), 3 (MAF < 0.050) [1]", default = "1") 
+parser.add_option("--locinfo", action = "store_true", dest = "locinfo", help = "include locus positional information (format = chromosome_position) in genotype matrix [FALSE]", default = False)
+parser.add_option("--refalt", action = "store_true", dest = "refalt", help = "include reference and alternative alleles in genotype matrix [FALSE]", default = False)
+parser.add_option("--headers", action = "store", dest = "headers", help = "specify which type of header to include in the genotype matrix (comma separated): 0 = none, 1 = matrix dimensions, 2 = sample IDs, 3 = population IDs, 4 = position and reference/alternative headers [1,2,3,4]", default = "1,2,3,4")
+parser.add_option("--delimit", action = "store", dest = "delimit", help = "specify which delimiter to use for the genotype matrix: 1 = space, 2 = tab [1]", default = "1")
 parser.add_option("--filvcf", action = "store", type = "string", dest = "filvcf", help ="specify a filtered VCF for genotyping (e.g., re-running a script) - bipasses creating new VCF [N/A]", default = "")
 
 options, args = parser.parse_args()
@@ -116,49 +121,73 @@ def vcf_filter():
 
 
 #################################################
-###          Creating Entropy output          ###
+###      Creating Genotype matrix output      ###
 #################################################
 
 ## Create input file for Entropy program using sample sheet and VCF
-def entropy(PL, filtered_vcf):
+def geno_matrix(PL, filtered_vcf, delimiter):
 	## Initialize output file
-	entropy_out = open(options.prefix+".entropy", "w")
+	genomatrix_out = open(options.prefix+".genomatrix", "w")
 	
 	## Get matrix dimensions (samples x loci) from VCFtools log
-	[samples, loci] = get_vcf_dims()
-	entropy_out.write(samples+" "+loci+" "+"1\n")
+	if "1" in options.headers:
+		[samples, loci] = get_vcf_dims()
+		genomatrix_out.write(samples+str(delimiter)+loci+str(delimiter)+"1\n")
 	
-	sample_total = 0
+	sample_total = file_len(options.sheet)
 	
 	## Output line of sample names from second column of sample sheet
-	for sline in open(options.sheet, "r"):
-		if not sline.strip().startswith("#"):
-			bar = sline.rstrip().split("\t")
-			l1out = bar[1]+" "
-			entropy_out.write(l1out)
-			sample_total += 1
+	if "2" in options.headers:
+		if options.locinfo is True:
+			if "4" in options.headers:
+				genomatrix_out.write("Marker"+delimiter)
+		if options.refalt is True:
+			if "4" in options.headers:
+				genomatrix_out.write("Ref."+delimiter+"Alt."+delimiter)
+		for sline in open(options.sheet, "r"):
+			if not sline.strip().startswith("#"):
+				bar = sline.rstrip().split("\t")
+				if "3" in options.genotype:
+					l1out = bar[1]+str(delimiter)
+				elif "0" or "1" or "2" in options.genotype:
+					l1out = bar[1]+str(delimiter)+bar[1]+str(delimiter)+bar[1]+str(delimiter)
+				genomatrix_out.write(l1out)
+		genomatrix_out.write("\n")
 	
 	## Output line of sample populations from third column of sample sheet
-	entropy_out.write("\n")
-	for sline in open(options.sheet, "r"):
-		if not sline.strip().startswith("#"):
-			bar = sline.rstrip().split("\t")
-			l2out = bar[2]+" "
-			entropy_out.write(l2out)
+	if "3" in options.headers:
+		if options.locinfo is True:
+			if "4" in options.headers:
+				genomatrix_out.write("Marker"+delimiter)
+		if options.refalt is True:
+			if "4" in options.headers:
+				genomatrix_out.write("Ref."+delimiter+"Alt."+delimiter)
+		for sline in open(options.sheet, "r"):
+			if not sline.strip().startswith("#"):
+				bar = sline.rstrip().split("\t")
+				if "3" in options.genotype:
+					l2out = bar[2]+str(delimiter)
+				elif "0" or "1" or "2" in options.genotype:
+					l2out = bar[2]+str(delimiter)+bar[2]+str(delimiter)+bar[2]+str(delimiter)
+				genomatrix_out.write(l2out)
+		genomatrix_out.write("\n")
 	
 	## Output genotypes for each sample from VCF (begin at column 10)
-	entropy_out.write("\n")
 	for vline in open(filtered_vcf, "r"):
 		if not vline.strip().startswith("#"):
 			bar = vline.rstrip().split("\t")
+			if options.locinfo is True:
+				genomatrix_out.write(bar[0]+"_"+bar[1]+str(delimiter))
+			if options.refalt is True:
+				genomatrix_out.write(bar[3]+str(delimiter)+bar[4]+str(delimiter))
 			for sample in range(9, sample_total+9):
 				vcfchunks = bar[sample].split(":")
-				geno_out = recode_gl(entropy_out, vcfchunks[PL])		# recode genotype likelihoods to genotypes
-				entropy_out.write('{:.5f}'.format(geno_out)+" ")		# to go 5 decimal points
-			entropy_out.write("\n")
+				geno_out = recode_gl(genomatrix_out, vcfchunks[PL], delimiter)		# recode genotype likelihoods user choice
+				genomatrix_out.write(geno_out+str(delimiter))
+			genomatrix_out.write("\n")
 	
-	entropy_out.close()
-	print "\n\n###Entropy results can be found in "+options.prefix+".entropy###\n\n"
+	genomatrix_out.close()
+	print "\n\n###The genotype likelihood matrix can be found in "+options.prefix+".genomatrix###\n\n"
 
 
 #################################################
@@ -262,10 +291,8 @@ def tri_fasta(GT, GQ, filtered_vcf):
 ## Standardize the likelihood (for each number alternative alleles): = likelihood/sum(all likelihoods)
 ## Multiple standardized likelihoods by number of alternative alles: = standardized likelihoods * # alternative alleles
 ## Sum to produce absolute genotype on 0 (homozygous reference) to 2 (homozygous alternative) scale
-def recode_gl(outfile, genochunk):
-	out = outfile
-	foo = genochunk
-	bar = foo.split(",")
+def recode_gl(outfile, genochunk, delimiter):
+	bar = genochunk.split(",")
 	p0 = float(10 ** (int(bar[0])/-10))
 	p1 = float(10 ** (int(bar[1])/-10))
 	p2 = float(10 ** (int(bar[2])/-10))
@@ -274,7 +301,16 @@ def recode_gl(outfile, genochunk):
 	g1 = float((p1/psum)*1)
 	g2 = float((p2/psum)*2)
 	gsum = float(round((g0 + g1 + g2),4))
-	return gsum
+	if options.genotype == "0":
+		return bar[0]+delimiter+bar[1]+delimiter+bar[2]
+	elif options.genotype == "1":
+		return '{:.3f}'.format(p0)+delimiter+'{:.3f}'.format(p1)+delimiter+'{:.3f}'.format(p2)
+	elif options.genotype == "2":
+		return '{:.3f}'.format(g0)+delimiter+'{:.3f}'.format(g1)+delimiter+'{:.3f}'.format(g2)
+	elif options.genotype == "3":
+		return '{:.5f}'.format(gsum)
+	else:
+		print "\n\n***Specify the output genotype format for genotype matrix***\n\n"
 		
 ## Determines which portion of the FORMAT column contains the genotype, PHRED genotype likelihood, and genotype quality
 ## Uses sample line from end of output VCF
@@ -337,6 +373,14 @@ def get_amb(major, minor):
 	else:
 		return "?"
 
+def file_len(fname):
+    count = 0
+    with open(fname) as f:
+        for line in f:
+        	if not line.startswith("#"):
+				count += 1
+    return count
+
 
 #################################################
 ###        		   Main Program               ###
@@ -356,8 +400,13 @@ def main():
 	(GT, PL, GQ) = get_stat(filtered_vcf)
 	
 	## If user specified Entropy output, give it to them
-	if options.entropy == True:
-		entropy(PL, filtered_vcf)
+	if options.genotype is not "":
+		if options.delimit == "1":
+			geno_matrix(PL, filtered_vcf, " ")
+		elif options.delimit == "2":
+			geno_matrix(PL, filtered_vcf, "\t")
+		else:
+			print "\n\n***Specify a delimiter for the genotype matrix!***\n\n"
 		print "\n\n***Creating genotype input for Entropy***\n\n"
 	else:
 		print "\n\n***Not creating any input for Entropy***\n\n"
